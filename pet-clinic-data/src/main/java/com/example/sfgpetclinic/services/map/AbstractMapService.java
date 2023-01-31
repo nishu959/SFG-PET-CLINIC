d4bab9be3c1cdcfca168ca0fdcf5b887e8a968c8
nisha <nishantkumarsingh098@gmail.com>
2023-01-31 13:28:34 +0530

    Autogenerated map id implementation

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,13 @@
 package com.example.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.sfgpetclinic.model.BaseEntity;
+import org.hibernate.engine.transaction.jta.platform.internal.JRun4JtaPlatform;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,8 +17,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+
+        if (object != null){
+            if(object.getId()==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object can't be null");
+        }
         return object;
     }
 
@@ -28,6 +36,16 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
     }
 
 
